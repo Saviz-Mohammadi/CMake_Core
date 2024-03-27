eb3dbf135d1885943a924f5b1380d2e0e55ac333
Saviz Mohammadi <savizmohammadi1379@gmail.com>
2024-03-27 11:01:00 -0700

    Made more improvements to the resources system

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 #include <main.hpp>
 
+/*! \mainpage My Personal Index Page
+ *
+ * \section intro_sec Introduction
+ *
+ * This is the introduction.
+ *
+ * \section install_sec Installation
+ *
+ * \subsection step1 Step 1: Opening the box
+ *
+ * etc...
+ */
+
+
 /** @file
  * This file contains the main function for the application.
  */
